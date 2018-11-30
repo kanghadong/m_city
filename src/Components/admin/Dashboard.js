214b21a7bbafac0597a5082d5dd4faf74e571475
kanghadong <hdkang2007@gmail.com>
2018-12-01 04:57:22 +0900

    m_city project

---
+++ src/Components/admin/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AdminLayout from '../../Hoc/AdminLayout';
+
+const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <div className="user_dashboard">
+        <div>
+          this is your dashboard
+        </div>
+      </div>
+    </AdminLayout>
+  );
+}
+
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './Resources/css/app.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+import { firebase } from './firebase';
+
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <Routes {...props} />
+    </BrowserRouter>
+  );
+}
+
+firebase.auth().onAuthStateChanged((user) =>  {
+  ReactDOM.render(<App user={user} />, document.getElementById('root'));
+});
